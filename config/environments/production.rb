c01890289bec1b402a7dd14ea7d3f33ab8f01311
helpfulsquash <kristen.hanny@gmail.com>
2014-07-11 12:32:45 -0400

    moving things around; updating my gmail password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'kristenandtoddgetmarried.heroku.com' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
@@ -91,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'kristenandtoddgetmarried.heroku.com' }
+  
 end
